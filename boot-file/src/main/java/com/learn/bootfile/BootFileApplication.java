8ce9213673f4f81af9d8c6cb12a4811061a093ff
wangyujie <979731285@qq.com>
2018-12-01 18:08:25 +0800

    spring-boot-learn

---
+++ boot-file/src/main/java/com/learn/bootfile/BootFileApplication.java
@@ -0,0 +1,12 @@
+package com.learn.bootfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootFileApplication.class, args);
+    }
+}
